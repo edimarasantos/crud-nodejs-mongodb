19f16acc572e0eac0750e9aca537678914d0474e
edimarasantos <edimara.dev@gmail.com>
2022-05-21 22:03:10 -0300

    first commit

---
+++ src/controllers/VideoController.js
@@ -0,0 +1,79 @@
+const { response } = require("express");
+const res = require("express/lib/response");
+const { v4: uuid } = require("uuid");
+const Video = require("../models/Video");
+
+module.exports = {
+    async index(request, response) {
+        try {
+            const videos = await Video.find();
+            return response.status(200).json({ videos });
+        } catch (err) {
+            response.status(500).json({ error: err.messagr });
+        }
+    },
+
+    async store(request, response) {
+        const { title, link } = request.body;
+
+        if (!title || !link) {
+            return response.status(400).json({ error: "Missing title or link" });
+        }
+
+        const video = new Video({
+            _id: uuid(),
+            title,
+            link,
+            liked: false,
+        })
+
+        try {
+            await video.save();
+    
+            return response.status(201).json({ message: "video added succesfully" });
+        } catch (err) {
+            response.status(500).json({ err: err.message});
+        }
+    },
+     
+    async update (request, response) {
+        const { title, link } = request.body;
+
+        if (!title && !link) {
+            return response.status(400).json({ error: "You must inform a new title or a new link"});
+        }
+
+        if (title) response.video.title = title;
+        if (link) response.video.link = link;
+
+        try {
+            await response.video.save();
+            return response.status(200).json({ error: "Video updated successfully!" });
+        } catch (err) {
+            response.status(500).json({ error: err.message });
+        }
+    },
+
+    async delete(request, response) {
+        try {
+            await response.video.remove();
+            return response.status(200).json({ error: "Video deleted successfully!" });
+
+        } catch (err) {
+            response.status(500).json({ error: err.message });
+        }
+    },
+
+    async updateLike (request, response) {
+        response.video.liked = !response.video.liked;
+
+        try {
+            await response.video.save();
+            return response.status(200).json({ message: `video ${response.video.liked ? "liked" : "unliked"} successfully`});
+
+        } catch (err) {
+            response.status(400).json({ error: err.message });
+        }
+    },
+
+}
